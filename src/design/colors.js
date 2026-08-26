ee7fc74f581894493161ad055054d8a6ea0cb35c
Gruber_Alexandra <alex.gruber100@gmail.com>
2026-08-26 14:26:11 +0300

    modified:   src/design/colors.js

---
+++ src/design/colors.js
@@ -0,0 +1,7 @@
+/* arhitectura proiectului -> best practices for files structures 
+all app flows
+mock endpoints for backend connection
+mock tutorials
+global state in react native, use state hooks
+
+*/
